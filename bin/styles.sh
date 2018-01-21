853da475937656de301bf4c9812ac9c2f42d20c8
Amit Dhamu <dev@amitd.co>
2018-01-21 16:35:18 +0000

    Use autoprefixer

--- bin/styles.sh
+++ bin/styles.sh
@@ -3,10 +3,13 @@
 set -x
 set -e
 
-SRC_FILE="styles.scss"
+FILE="styles"
+IN_EXT="scss"
+OUT_EXT="css"
 SRC_DIRECTORY="src/scss"
-OUTPUT_FILE="styles.min.css"
 OUTPUT_DIRECTORY="dist/css"
 
-rm -f "${OUTPUT_DIRECTORY}/${OUTPUT_FILE}"
-node-sass --include-path scss "${SRC_DIRECTORY}/${SRC_FILE}" "${OUTPUT_DIRECTORY}/${OUTPUT_FILE}"
+rm -f "${OUTPUT_DIRECTORY}/${FILE}.${OUT_EXT}"
+
+node-sass --output-style compressed --omit-source-map-url -o ${OUTPUT_DIRECTORY} "${SRC_DIRECTORY}/${FILE}.${IN_EXT}"
+postcss -u autoprefixer -r "${OUTPUT_DIRECTORY}/*" --no-map
