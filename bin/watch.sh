8a1d19838e5ce0b1327506e6907558a8d41d988c
Amit Dhamu <dev@amitd.co>
2018-01-28 16:44:03 +0000

    Added http-server

--- bin/watch.sh
+++ bin/watch.sh
@@ -6,4 +6,5 @@ set -e
 concurrently \
     "chokidar 'src/**/*.{css,scss}' -c 'yarn styles && yarn html'" \
     "chokidar 'src/**/*.js' -c 'yarn scripts && yarn html'" \
-    "chokidar 'src/**/*.{jpg,jpeg,gif,png,svg,JPG,JPEG,GIF,PNG,SVG}' -c 'yarn images && yarn html'"
+    "chokidar 'src/**/*.{jpg,jpeg,gif,png,svg,JPG,JPEG,GIF,PNG,SVG}' -c 'yarn images && yarn html'" \
+    "http-server"
